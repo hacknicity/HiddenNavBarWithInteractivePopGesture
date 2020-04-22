91beb1aa07fa54c6145263d768b3e4f69463c310
Geoff Hackworth <geoff@hacknicity.com>
2020-04-22 09:57:28 +0100

    Initial Commit. Xcode 11.4.1's Single View App template. Removed my Team name from the Signing & Capabilities section.

---
+++ HiddenNavBarWithInteractivePopGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiddenNavBarWithInteractivePopGesture
+//
+//  Created by Geoff Hackworth on 22/04/2020.
+//  Copyright © 2020 Hacknicity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
